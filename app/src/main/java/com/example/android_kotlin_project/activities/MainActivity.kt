b9f7b7efaf15ddf338639015bb416c3f21ebc78e
3cvms3 <cvms333@yahoo.com>
2024-11-28 10:00:20 +0100

    Settings Fragment: button dark mode and choose langage without back

--- app/src/main/java/com/example/android_kotlin_project/activities/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_project/activities/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.android_kotlin_project.activities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -13,37 +12,28 @@ import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
 
-    /*private fun hideBottomNavigationLabels(bottomNavigationView: BottomNavigationView) {
-        for (i in 0 until bottomNavigationView.menu.size()) {
-            val item = bottomNavigationView.menu.getItem(i)
-            item.setTitle("")
-        }
-    }*/
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        val bottomNavigationView  = findViewById<BottomNavigationView>(R.id.navigation)
-        //hideBottomNavigationLabels(bottomNavigationView)
-
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_fragment) as NavHostFragment
         val navController: NavController = navHostFragment.navController
-
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
         FirebaseApp.initializeApp(this)
 
-        /*
-        val signupButton: Button = findViewById(R.id.button_test)
-        signupButton.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
-        }*/
+        val settingsIcon = findViewById<View>(R.id.settings_icon)
+        val settingsPopup = findViewById<View>(R.id.settings_popup)
+        val settingsPopupText = findViewById<View>(R.id.settings_popup_text)
+
+        settingsIcon.setOnClickListener {
+            settingsPopup.visibility = if (settingsPopup.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        settingsPopupText.setOnClickListener {
+            settingsPopup.visibility = View.GONE
+            navController.navigate(R.id.settingsFragment)
+        }
     }
 }
--- app/src/main/java/com/example/android_kotlin_project/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/android_kotlin_project/fragments/SettingsFragment.kt
@@ -1,4 +1,19 @@
 package com.example.android_kotlin_project.fragments
 
-class SettingsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android_kotlin_project.R
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
 }
