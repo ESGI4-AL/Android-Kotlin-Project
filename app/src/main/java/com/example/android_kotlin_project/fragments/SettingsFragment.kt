2788fd94b549a24eabc4755d583e19d09bde8fc9
3cvms3 <cvms333@yahoo.com>
2024-12-16 14:12:48 +0100

    button back working

--- app/src/main/java/com/example/android_kotlin_project/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/android_kotlin_project/fragments/SettingsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.example.android_kotlin_project.R
 
@@ -15,5 +16,14 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val backButton: ImageButton = view.findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
     }
 }
