8e0e648d9ae09cd9a97622a2e2479e867714a0b4
3cvms3 <cvms333@yahoo.com>
2024-12-19 11:16:54 +0100

    rebase homeFragmenttoJournalFragment: card clickable

---
+++ app/src/main/java/com/example/android_kotlin_project/fragments/JournalFragment.kt
@@ -0,0 +1,28 @@
+package com.example.android_kotlin_project.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android_kotlin_project.databinding.FragmentJournalBinding
+
+class JournalFragment : Fragment() {
+
+    private var _binding: FragmentJournalBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // binding for JournalFragment
+        _binding = FragmentJournalBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
