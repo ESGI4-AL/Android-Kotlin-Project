bbdf1d8dd7570f89cc1f6019f5ac60be1ffda9d0
3cvms3 <cvms333@yahoo.com>
2024-12-22 13:56:03 +0100

    back button functionnality working

--- app/src/main/java/com/example/android_kotlin_project/fragments/JournalFragment.kt
+++ app/src/main/java/com/example/android_kotlin_project/fragments/JournalFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import com.example.android_kotlin_project.R
 import com.example.android_kotlin_project.databinding.FragmentJournalBinding
 
 class JournalFragment : Fragment() {
@@ -16,9 +18,18 @@ class JournalFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //binding for JournalFragment
+        //binding
         _binding = FragmentJournalBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val backButton: ImageButton = view.findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
     }
 
     override fun onDestroyView() {
