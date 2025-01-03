8606f3022dfe5c4bcdb96af4bd33c652f922c6f8
Celina-cbn <chabcelina@gmail.com>
2025-01-03 01:33:25 +0100

    dark mode first commit

--- app/src/main/java/com/example/android_kotlin_project/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/android_kotlin_project/fragments/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.android_kotlin_project.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.android_kotlin_project.R
 
@@ -21,9 +24,28 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val themeSwitch = view.findViewById<Switch>(R.id.theme_switch)
+
         val backButton: ImageButton = view.findViewById(R.id.back_button)
         backButton.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+
+        val sharedPreferences = requireActivity().getSharedPreferences("AppSettings", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPreferences.getBoolean("dark_mode", false)
+        themeSwitch.isChecked = isDarkMode
+
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            val editor = sharedPreferences.edit()
+
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("dark_mode", true)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("dark_mode", false)
+            }
+            editor.apply()
         }
     }
 }
