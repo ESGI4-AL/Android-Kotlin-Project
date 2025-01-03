7c558ac37d5793968bc5b3802319d64c964d1971
Celina-cbn <chabcelina@gmail.com>
2025-01-03 01:33:25 +0100

    dark mode first commit

--- app/src/main/java/com/example/android_kotlin_project/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/android_kotlin_project/fragments/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.android_kotlin_project.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,8 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ImageButton
 import android.widget.Spinner
-import android.content.Context
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.android_kotlin_project.R
 import com.example.android_kotlin_project.adapters.FlagSpinnerAdapter
@@ -29,9 +31,28 @@ class SettingsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setupLanguageSpinner(view)
 
+        val themeSwitch = view.findViewById<Switch>(R.id.theme_switch)
+
         val backButton: ImageButton = view.findViewById(R.id.back_button)
         backButton.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+
+        val sharedPreferences = requireActivity().getSharedPreferences("AppSettings", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPreferences.getBoolean("dark_mode", false)
+        themeSwitch.isChecked = isDarkMode
+
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            val editor = sharedPreferences.edit()
+
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("dark_mode", true)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("dark_mode", false)
+            }
+            editor.apply()
         }
     }
 
