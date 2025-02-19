b6f75182fc5fe612695b10dda04be397019e8ade
Celina-cbn <chabcelina@gmail.com>
2025-02-19 20:35:47 +0100

    version without database

--- app/src/main/java/com/example/android_kotlin_project/fragments/ActivityFragment.kt
+++ app/src/main/java/com/example/android_kotlin_project/fragments/ActivityFragment.kt
@@ -1,31 +1,47 @@
 package com.example.android_kotlin_project.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import android.widget.Toast
 import com.example.android_kotlin_project.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ActivityFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ActivityFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_activity, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+      //  val fullBodyButton = view.findViewById<Button>(R.id.start_yoga)
+        val yogaButton = view.findViewById<Button>(R.id.start_yoga)
+     //   val cardioButton = view.findViewById<Button>(R.id.start_cardio)
+        val scaleUpAnimation = AnimationUtils.loadAnimation(context, R.anim.scale_up)
+
+
+    /*    fullBodyButton.setOnClickListener {
+            it.startAnimation(scaleUpAnimation)
+            Toast.makeText(context, "Full Body Workout commencé !", Toast.LENGTH_SHORT).show()
+        }
+
+     */
+
+        yogaButton.setOnClickListener {
+            Toast.makeText(context, "Yoga Session commencée !", Toast.LENGTH_SHORT).show()
+        }
+
+ /*       cardioButton.setOnClickListener {
+            Toast.makeText(context, "Cardio Blast commencé !", Toast.LENGTH_SHORT).show()
+        }
+
+  */
     }
 }
