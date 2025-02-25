5e1e388347205ae65ac02de0fe58f303c7f9b213
Celina-cbn <chabcelina@gmail.com>
2025-02-25 21:42:28 +0100

    filter actu

--- app/src/main/java/com/example/android_kotlin_project/api/NewsApiService.kt
+++ app/src/main/java/com/example/android_kotlin_project/api/NewsApiService.kt
@@ -6,9 +6,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApiService {
-    @GET("v2/top-headlines")
+    @GET("v2/everything")
     fun getNews(
-        @Query("category") category: String = "health",
+        @Query("q") query: String = "fitness OR yoga OR sport",
+        @Query("language") language: String = "fr",
         @Query("apiKey") apiKey: String = "6ed0727d4bc0465e8a3dab25253f3865"
     ): Call<NewsResponse>
 }
