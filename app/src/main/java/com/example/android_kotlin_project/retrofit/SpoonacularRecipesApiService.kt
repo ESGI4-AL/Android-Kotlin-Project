abcb446d836221f5374b98ff5930610e4eb11b5d
soniaaaloui2 <soniaaloui240@gmail.com>
2025-03-01 07:33:32 +0100

    fixes

--- app/src/main/java/com/example/android_kotlin_project/retrofit/SpoonacularRecipesApiService.kt
+++ app/src/main/java/com/example/android_kotlin_project/retrofit/SpoonacularRecipesApiService.kt
@@ -8,10 +8,13 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SpoonacularRecipesApiService {
+    companion object {
+        const val API_KEY = "1c4aa20d8f964caea36e1886cc9a380c"
+    }
     @GET("recipes/random")
      fun getRandomRecipe (
         //hardcoded but needs to be a secured var env
-        @Query("apiKey") apiKey: String="1c4aa20d8f964caea36e1886cc9a380c",
+        @Query("apiKey") apiKey: String= API_KEY,
         @Query("number") number: Int=1
     ): Call<RecipeListDto>
 
@@ -19,21 +22,21 @@ interface SpoonacularRecipesApiService {
     @GET("recipes/{id}/information")
     fun getRecipeById(
         @Path("id") recipeId: Int,
-        @Query("apiKey") apiKey: String="1c4aa20d8f964caea36e1886cc9a380c",
+        @Query("apiKey") apiKey: String= API_KEY,
         @Query("includeNutrition") includeNutrition: Boolean = false
 
     ): Call<RecipeDto>
 
     @GET("recipes/random")
     fun getRandomRecipes(
-        @Query("apiKey") apiKey: String="1c4aa20d8f964caea36e1886cc9a380c",
+        @Query("apiKey") apiKey: String= API_KEY,
         @Query("number") number: Int=10
     ): Call<RecipeListDto>
 
 
     @GET("recipes/random")
     fun getRecipesByCategory(
-        @Query("apiKey") apiKey: String="1c4aa20d8f964caea36e1886cc9a380c",
+        @Query("apiKey") apiKey: String= API_KEY,
         @Query("number") number: Int=20,
         @Query("tags") tags: String? = null
 
